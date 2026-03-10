902aab131762691f17c656a4c0a7339e61d4c9d2
Alexander Furkalo <haktoid@ukr.net>
2026-03-10 20:26:38 +0200

    Lab5-6

---
+++ Lab5-6-ForTask5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_6_ForTask5
+{
+    public interface IMessageSender
+    {
+        void Send(string message, string recipient);
+    }
+
+    public class EmailSender : IMessageSender
+    {
+        public void Send(string message, string recipient)
+        {
+            Console.WriteLine($"[EMAIL] Sending message to {recipient}: {message}");
+        }
+    }
+
+    public class SMSsender
+    {
+        public void SendSMS(string phoneNumber, string text)
+        {
+            Console.WriteLine($"[SMS] Sending SMS to {phoneNumber}: {text}");
+        }
+    }
+
+    public class SMSAdapter : IMessageSender
+    {
+        private SMSsender smsSender;
+
+        public SMSAdapter()
+        {
+            smsSender = new SMSsender();
+        }
+
+        public void Send(string message, string recipient)
+        {
+            smsSender.SendSMS(recipient, message);
+        }
+    }
+
+    public class TelegramSender
+    {
+        public void SendTelegramMessage(string username, string message)
+        {
+            Console.WriteLine($"[Telegram] Sending Telegram message to {username}: {message}");
+        }
+    }
+
+    public class TelegramAdapter : IMessageSender
+    {
+        private TelegramSender telegramSender;
+
+        public TelegramAdapter()
+        {
+            telegramSender = new TelegramSender();
+        }
+
+        public void Send(string message, string recipient)
+        {
+            telegramSender.SendTelegramMessage(recipient, message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IMessageSender sender;
+
+            sender = new EmailSender();
+            sender.Send("Привіт! Відправляю тобі лист!", "user@email.com");
+
+            sender = new SMSAdapter();
+            sender.Send("Вітаю через смс!", "+380123456789");
+
+            sender = new TelegramAdapter();
+            sender.Send("Надсилаю повідомлення через Telegram!", "@username");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab5-6-ForTask6/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Lab5_6_ForTask6
+{
+    /// Subject
+    public interface IWebServer
+    {
+        string GetPage(string url);
+    }
+
+    /// RealSubject
+    public class RealWebServer : IWebServer
+    {
+        public string GetPage(string url)
+        {
+            Console.WriteLine("Fetching page from real web server...");
+            Thread.Sleep(1500); 
+
+            return $"<html>Content of {url}</html>";
+        }
+    }
+
+    /// Proxy
+    public class ProxyWebServer : IWebServer
+    {
+        private RealWebServer realServer;
+        private Dictionary<string, string> cache;
+
+        public ProxyWebServer()
+        {
+            cache = new Dictionary<string, string>();
+        }
+
+        public string GetPage(string url)
+        {
+            if (cache.ContainsKey(url))
+            {
+                Console.WriteLine("Returning cached page...");
+                return cache[url];
+            }
+
+            Console.WriteLine("Page not in cache. Fetching from server...");
+
+            if (realServer == null)
+                realServer = new RealWebServer();
+
+            string page = realServer.GetPage(url);
+
+            cache[url] = page;
+
+            return page;
+        }
+    }
+
+    /// Client
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebServer server = new ProxyWebServer();
+
+            Console.WriteLine(server.GetPage("example.com"));
+            Console.WriteLine();
+
+            Console.WriteLine(server.GetPage("example.com"));
+            Console.WriteLine();
+
+            Console.WriteLine(server.GetPage("secondpage.com"));
+
+            Console.ReadKey();
+        }
+    }
+}
