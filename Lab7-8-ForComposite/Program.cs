b4c7dc26f2d7a1e0215499481dc4836882767977
Alexander Furkalo <haktoid@ukr.net>
2026-03-17 16:32:12 +0200

    Lab7-8 (first part)

---
+++ Lab7-8-ForComposite/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_8_ForComposite
+{
+    abstract class Shape
+    {
+        public abstract void Draw();
+
+        public abstract void Add(Shape shape);
+
+        public abstract void Remove(Shape shape);
+    }
+
+    class CompositeShape : Shape
+    {
+        private List<Shape> children = new List<Shape>();
+
+        public override void Add(Shape shape)
+        {
+            children.Add(shape);
+        }
+
+        public override void Remove(Shape shape)
+        {
+            children.Remove(shape);
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Composite Shape:");
+
+            foreach (Shape shape in children)
+            {
+                shape.Draw();
+            }
+        }
+    }
+
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Circle");
+        }
+
+        public override void Add(Shape shape)
+        {
+            Console.WriteLine("Cannot add.");
+        }
+
+        public override void Remove(Shape shape)
+        {
+            Console.WriteLine("Cannot remove.");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+
+        public override void Add(Shape shape)
+        {
+            Console.WriteLine("Cannot add.");
+        }
+
+        public override void Remove(Shape shape)
+        {
+            Console.WriteLine("Cannot remove.");
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing Triangle");
+        }
+
+        public override void Add(Shape shape)
+        {
+            Console.WriteLine("Cannot add.");
+        }
+
+        public override void Remove(Shape shape)
+        {
+            Console.WriteLine("Cannot remove.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення фігур
+            Shape circle = new Circle();
+            Shape rectangle = new Rectangle();
+            Shape triangle = new Triangle();
+
+            // Група фігур 1
+            CompositeShape group1 = new CompositeShape();
+            group1.Add(circle);
+            group1.Add(rectangle);
+
+            // Група фігур 2, об'єднання
+            CompositeShape group2 = new CompositeShape();
+            group2.Add(triangle);
+            group2.Add(group1);
+
+            group2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab7-8-ForDecorator/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_8_ForDecorator
+{
+    abstract class IceCream
+    {
+        public abstract string GetDescription();
+        public abstract double GetCost();
+    }
+
+    class SimpleIceCream : IceCream
+    {
+        public override string GetDescription()
+        {
+            return "Vanilla Ice Cream";
+        }
+
+        public override double GetCost()
+        {
+            return 2.0;
+        }
+    }
+
+    abstract class IceCreamDecorator : IceCream
+    {
+        protected IceCream iceCream;
+
+        public IceCreamDecorator(IceCream iceCream)
+        {
+            this.iceCream = iceCream;
+        }
+
+        public override string GetDescription()
+        {
+            return iceCream.GetDescription();
+        }
+
+        public override double GetCost()
+        {
+            return iceCream.GetCost();
+        }
+    }
+
+    class ChocolateDecorator : IceCreamDecorator
+    {
+        public ChocolateDecorator(IceCream iceCream) : base(iceCream)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return iceCream.GetDescription() + ", Chocolate";
+        }
+
+        public override double GetCost()
+        {
+            return iceCream.GetCost() + 0.7;
+        }
+    }
+
+    class NutsDecorator : IceCreamDecorator
+    {
+        public NutsDecorator(IceCream iceCream) : base(iceCream)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return iceCream.GetDescription() + ", Nuts";
+        }
+
+        public override double GetCost()
+        {
+            return iceCream.GetCost() + 0.8;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IceCream iceCream = new SimpleIceCream();
+
+            Console.WriteLine(iceCream.GetDescription() + " | Cost: " + iceCream.GetCost());
+
+            iceCream = new ChocolateDecorator(iceCream);
+
+            Console.WriteLine(iceCream.GetDescription() + " | Cost: " + iceCream.GetCost());
+
+            iceCream = new NutsDecorator(iceCream);
+
+            Console.WriteLine(iceCream.GetDescription() + " | Cost: " + iceCream.GetCost());
+
+            Console.ReadLine();
+        }
+    }
+}
