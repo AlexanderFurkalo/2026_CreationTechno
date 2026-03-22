32485d8428b2f820dcf6148da863fcf5bc8e4132
Alexander Furkalo <haktoid@ukr.net>
2026-03-23 00:05:41 +0200

    7-8-PartTwo

---
+++ Lab7-8-ForFacade/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_8_ForFacade
+{
+    class CPU
+    {
+        public void Freeze()
+        {
+            Console.WriteLine("CPU: Freezing processor...");
+        }
+
+        public void Jump(long position)
+        {
+            Console.WriteLine($"CPU: Jumping to {position}...");
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("CPU: Executing instructions...");
+        }
+    }
+
+    class Memory
+    {
+        public void Load(long position, string data)
+        {
+            Console.WriteLine($"Memory: Loading data '{data}' into position {position}...");
+        }
+    }
+
+    class HardDrive
+    {
+        public string Read(long lba, int size)
+        {
+            Console.WriteLine($"HardDrive: Reading {size} bytes from sector {lba}...");
+            return "BOOT_DATA";
+        }
+    }
+
+    class ComputerFacade
+    {
+        private CPU cpu;
+        private Memory memory;
+        private HardDrive hardDrive;
+
+        public ComputerFacade()
+        {
+            cpu = new CPU();
+            memory = new Memory();
+            hardDrive = new HardDrive();
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("Starting computer...\n");
+
+            cpu.Freeze();
+
+            string bootData = hardDrive.Read(0, 1024);
+
+            memory.Load(0, bootData);
+
+            cpu.Jump(0);
+
+            cpu.Execute();
+
+            Console.WriteLine("\nComputer started successfully!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerFacade computer = new ComputerFacade();
+            computer.TurnOn();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab7-8-ForFlyweight/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_8_ForFlyweight
+{
+    class BookFlyweight
+    {
+        public string Title { get; }
+        public string Author { get; }
+
+        public BookFlyweight(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public void Display(int id, string shelf)
+        {
+            Console.WriteLine($"Book ID: {id}, Title: {Title}, Author: {Author}, Shelf: {shelf}");
+        }
+    }
+
+    class BookFactory
+    {
+        private Dictionary<string, BookFlyweight> books = new Dictionary<string, BookFlyweight>();
+
+        public BookFlyweight GetBook(string title, string author)
+        {
+            string key = title + "_" + author;
+
+            if (!books.ContainsKey(key))
+            {
+                Console.WriteLine("Creating new book flyweight...");
+                books[key] = new BookFlyweight(title, author);
+            }
+
+            return books[key];
+        }
+    }
+    class Book
+    {
+        private int id;
+        private string shelf;
+        private BookFlyweight flyweight;
+
+        public Book(int id, string shelf, BookFlyweight flyweight)
+        {
+            this.id = id;
+            this.shelf = shelf;
+            this.flyweight = flyweight;
+        }
+
+        public void Display()
+        {
+            flyweight.Display(id, shelf);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookFactory factory = new BookFactory();
+
+            List<Book> library = new List<Book>();
+
+            library.Add(new Book(1, "A1", factory.GetBook("1984", "George Orwell")));
+            library.Add(new Book(2, "A2", factory.GetBook("Flowers for Algernon", "Daniel Keyes")));
+            library.Add(new Book(3, "B1", factory.GetBook("The Colour Out of Space", "Howard Phillips Lovecraft")));
+            library.Add(new Book(4, "B2", factory.GetBook("1984", "George Orwell")));
+            library.Add(new Book(5, "B3", factory.GetBook("1984", "George Orwell")));
+
+            foreach (var book in library)
+            {
+                book.Display();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
